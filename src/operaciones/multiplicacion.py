c0850276bee697b5de78fcb84de1b77b6d1b6b2c
Sonia Avilés Sacoto <sonia_v_a_s@hotmail.com>
2025-11-28 23:26:06 -0500

    Multiplicación

---
+++ src/operaciones/multiplicacion.py
@@ -0,0 +1,9 @@
+'''Funcion para multiplicar dos numeros'''
+def multiplicacion(a:float,b:float) -> float: 
+    '''Devuelve la multiplicacion de dos numeros
+    a: Primer numero
+    b: Segundo numero
+    Returns:
+    Devuelve resultado de la multiplicacion de dos numeros a*b  
+        '''
+    return a*b
