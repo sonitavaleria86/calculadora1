c3dbd608c470bfd60c6f70003408ee88538a0fb1
benjaminfl <josebenjaminfloreslopez@outlook.com>
2025-11-28 21:06:52 -0600

    Añade módulo de suma

---
+++ src/operaciones/suma.py
@@ -0,0 +1,9 @@
+'''Funcion para sumar dos numeros'''
+def suma(a:float,b:float) -> float:
+    '''Devuelve la suma de dos numeros
+    a: Primer numero 
+    b: Segundo numero 
+    Returns:
+    Devuelve resultado de la suma de a+b 
+        '''
+    return a+b 
