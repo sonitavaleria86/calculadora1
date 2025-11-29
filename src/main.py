1c61e04b4b959b1d51deaaddd4ce2068d4efee62
AlexisTec <alexisnu_fc@ciencias.unam.mx>
2025-11-29 07:26:33 -0600

    Se agrega la funcionalidad de modulo

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from operaciones import suma, resta, multiplicacion, division, potencia
+from operaciones import suma, resta, multiplicacion, division, potencia, modulo
 
 def menu():
     """
@@ -11,39 +11,56 @@ def menu():
         print("Presione 3 si desea realizar una: MULTIPLICACION")
         print("Presione 4 si desea realizar una: DIVISION")
         print("Presione 5 si desea realizar una: POTENCIA")
-        print("Presione 6 si desea SALIR del programa")
+        print("Presione 6 si desea realizar un: MODULO")
+        print("Presione 7 si desea SALIR del programa")
 
-        opcion = input("Seleccione una opcion del 1 al 6: ")
+        '''Solicita al usuario la eleccion de una opcion'''
+        opcion = input("Seleccione una opcion del 1 al 7: ")
 
-        if opcion == "6":
+        '''Si el usuario eligio la opcion 5, el programa se cierra'''
+        if opcion == "7":
             print("Saleindo de calculadora...")
             break
-
-        if opcion not in ["1", "2", "3", "4", "5"]:
+        
+            '''Si el usuario elige una opcion diferente a las mostradas en el menu: se imprime un aviso'''
+        if opcion not in ["1", "2", "3", "4", "5", "6"]:
             print("\n --AVISO: La opcion seleccionada NO EXISTE:")
             print("Vuelva a intentar con una opcion del 1 al 5--")
             continue
 
-        # Captura los valores de a y b que decidio el usuario
+
+        # El programa captura los valores de a(flotante) y b(flotante) que proporciono el usuario
         try:
+            '''El usuario ingresa los valores (a y b)'''
             a = float(input("Ingresa el primer valor: "))
             b = float(input("Ingresa el segundo valor: "))
+            '''Si el usuario no ingresa un numero de manera correcta. El programa imprime un aviso'''
         except ValueError:
             print("\n --Error: Alguno o algunos de los valores que ingresaste no es valido")
             print("Intenta con un valor de tipo FLOTANTE--")
             continue
 
-        # Operaciones 
+# Operaciones 
+        '''Si el usuario selecciona la opcion 1, entonces el programa realiza una suma'''
+
+#Si el usuario selecciona la opcion 1, entonces el programa realiza una suma
         if opcion == "1":
             print("Resultado:", suma(a, b))
+#Si el usuario selecciona la opcion 2, entonces el programa realiza una resta
         elif opcion == "2":
             print("Resultado:", resta(a, b))
+#Si el usuario selecciona la opcion 3, entonces el programa realiza una multiplicacion
         elif opcion == "3":
             print("Resultado:", multiplicacion(a, b))
+#Si el usuario selecciona la opcion 4, entonces el programa realiza una division con decorador
         elif opcion == "4":
             print("Resultado:", division(a, b))
+#Si el usuario selecciona la opcion 5, entonces el programa realiza una potencia
         elif opcion == "5":
             print("Resultado:", potencia(a, b))
+ #Si el usuario selecciona la opcion 6, entonces el programa obtiene el modulo de un numero
+        elif opcion == "6":
+            print("Resultado:", modulo(a, b))
 
 
 # Llama al menú
